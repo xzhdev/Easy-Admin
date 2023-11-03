c514cf5a77a219f4dc683cfe0638943dae17948d
熊支辉 <xiongzhihui@swordgate.cn>
2023-11-03 16:38:51 +0800

    feat(生产环境控制台禁止调试): 🚀 block debugger

--- src/App.vue
+++ src/App.vue
@@ -10,11 +10,11 @@ import { useI18n } from "vue-i18n";
 import { getBrowserLang } from "@/utils";
 import { useTheme } from "@/hooks/useTheme";
 import { ElConfigProvider } from "element-plus";
-
 import { LanguageType } from "./stores/interface";
 import { useGlobalStore } from "@/stores/modules/global";
 import en from "element-plus/es/locale/lang/en";
 import zhCn from "element-plus/es/locale/lang/zh-cn";
+import Language from "./layouts/components/Header/components/Language.vue";
 
 const globalStore = useGlobalStore();
 
@@ -42,4 +42,41 @@ const assemblySize = computed(() => globalStore.assemblySize);
 
 // element button config
 const buttonConfig = reactive({ autoInsertSpace: false });
+
+// block debugger
+process.env.NODE_ENV === "production" &&
+  (function block() {
+    let currentTime = new Date().getTime();
+    const tips = {
+      zh: "禁止调试，请重新访问！",
+      en: "Please do not debug, please revisit!"
+    };
+    // setInterval版
+    // const timeout = setInterval(() => {
+    //   new Function("debugger")();
+    //   const newTime = new Date().getTime();
+    //   if (newTime - currentTime > 500) {
+    //     clearInterval(timeout);
+    //     window.location.href = "about:blank";
+    //   } else {
+    //     currentTime = newTime;
+    //   }
+    // }, 400);
+    //递归 setTimeout
+    function timeoutFun() {
+      const timeout = setTimeout(() => {
+        new Function("debugger")();
+        const newTime = new Date().getTime();
+        if (newTime - currentTime > 500) {
+          clearInterval(timeout);
+          alert(tips[globalStore.language!]);
+          window.close();
+        } else {
+          currentTime = newTime;
+        }
+        timeoutFun();
+      }, 400);
+    }
+    timeoutFun();
+  })();
 </script>
